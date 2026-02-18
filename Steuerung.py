a2575f4efbb708294257be01b8f7ad94e933c355
Jonas <jonas.gasparini@outlook.de>
2026-02-18 09:05:42 +0100

    Test 13.02.2025 position 1-4 Versuch 5 Position danach vollständige Rotation Motor 1&2

--- Steuerung.py
+++ Steuerung.py
@@ -179,7 +179,7 @@ if __name__ == "__main__":
 
             if cmd in "01234":
                 pos = int(cmd)
-                move_motors_simultaneously(motors, "move_to5_position", pos, delay)
+                move_motors_simultaneously(motors, "move_to_position", pos, delay)
                 time.sleep(1)
                 motor1.rotate_steps(200,0.005,True)
                 move_motors_simultaneously(motors, "move_to_home", delay)
