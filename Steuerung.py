fb4de8298878e297ec75fb287d2e502f49f05d6b
Jonas <jonas.gasparini@outlook.de>
2026-02-18 10:21:49 +0100

    Test 13.02.2025 position 1-4 Versuch 8 Position danach vollständige Rotation Motor 1&2

--- Steuerung.py
+++ Steuerung.py
@@ -152,7 +152,7 @@ def get_char():
         termios.tcsetattr(fd, termios.TCSADRAIN, old)
 
 def Kamera_erkennung():
-    print("📸 Starte Kamera und Erkennung...")
+    print("Starte Kamera und Erkennung...")
 
     result = subprocess.run(
         [sys.executable, Kamera_Script],
@@ -163,16 +163,16 @@ def Kamera_erkennung():
     print(f"Kamera-Ausgabe:\n{result.stdout}")
 
     if result.returncode != 0:
-        print(f"❌ Fehler bei Kamera/CNN:\n{result.stderr}")
+        print(f"Fehler bei Kamera/CNN:\n{result.stderr}")
         return None
 
     for line in result.stdout.splitlines():
         if line.startswith("Kategorie:"):
             kategorie = line.split(":", 1)[1].strip().lower()
-            print(f"✅ Kategorie erkannt: {kategorie}")
+            print(f"Kategorie erkannt: {kategorie}")
             return kategorie
 
-    print("⚠️ Keine Kategorie in der Ausgabe gefunden.")
+    print("Keine Kategorie in der Ausgabe gefunden.")
     return None
 
 
@@ -194,18 +194,18 @@ if __name__ == "__main__":
                 kategorie = Kamera_erkennung()
                 if kategorie:
                     position = Kategorie_zu_Positonen.get(kategorie, 0)
-                    print(f"🔄 '{kategorie}' → Position {position}")
+                    print(f" '{kategorie}' → Position {position}")
                     move_motors_simultaneously(motors, "move_to_position", position, delay)
-                    print("⏳ Warte 1 Sekunde...")
+                    print("Warte 1 Sekunde...")
                     time.sleep(1)
                     print("🔧 Auswurf...")
                     motor1.rotate_steps(200, 0.005, True)
                     time.sleep(1)
-                    print("🏠 Fahre zurück zur Home-Position...")
+                    print("Fahre zurück zur Home-Position...")
                     move_motors_simultaneously(motors, "move_to_home", delay)
-                    print("✅ Fertig!")
+                    print("Fertig!")
                 else:
-                    print("❌ Keine Kategorie erkannt, Motor bleibt stehen.")
+                    print("Keine Kategorie erkannt, Motor bleibt stehen.")
 
             elif cmd in "01234":
                 pos = int(cmd)
